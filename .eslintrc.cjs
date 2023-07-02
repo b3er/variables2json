0123a7c8f32dc11f9b346defdd75008e952654c0
Mark <mark@mooibroek.dev>
2023-07-02 16:26:17 +0200

    better linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,17 @@
 module.exports = {
   env: {
-    node: true,
+    node: true
   },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
   parser: "vue-eslint-parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
-    parser: "@typescript-eslint/parser",
+    parser: "@typescript-eslint/parser"
   },
   plugins: ["@typescript-eslint"],
   rules: {
     "vue/multi-word-component-names": "off",
-    "no-unused-vars": "off",
-  },
+    "no-unused-vars": "off"
+  }
 };
